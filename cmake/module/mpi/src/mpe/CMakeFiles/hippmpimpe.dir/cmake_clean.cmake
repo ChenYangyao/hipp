a52dc52d8ee5a43c1dfb15c59021f064e2a5be82
chenyangyao <652415049@qq.com>
2020-03-16 16:57:45 +0800

    2020-03-16 Mon 16:57:45

--- cmake/module/mpi/src/mpe/CMakeFiles/hippmpimpe.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hippmpimpe.dir/mpe_sync_seqblock.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/hippmpimpe.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake/module/numerical/src/function/CMakeFiles/hippnumericalfunction.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hippnumericalfunction.dir/integration.cpp.o"
-  "CMakeFiles/hippnumericalfunction.dir/interp.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/hippnumericalfunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

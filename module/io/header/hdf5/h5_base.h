3ffd0855380908a297c455e403b0c1bfbedfbb53
Yangyao Chen <yangyaochen.astro@foxmail.com>
2020-11-29 01:28:35 -0500

    move class-specific typedefs outwards

--- module/io/header/hdf5/h5_base.h
+++ module/io/header/hdf5/h5_base.h
@@ -2,15 +2,23 @@
  * creat: Yangyao CHEN, 2020/01/11
  * Base file for including in all file in the hdf5 module.
  */ 
-
-
 #ifndef _HIPPIO_H5_BASE_H_
 #define _HIPPIO_H5_BASE_H_
 #include <hdf5.h>
 #include <hippcntl.h>
-namespace HIPP{
-namespace IO{
+
+namespace HIPP {
+namespace IO {
+
+typedef std::size_t size_t;
+typedef hid_t id_t;
+typedef hsize_t hsize_t;
+typedef hssize_t hssize_t;
+typedef herr_t err_t;
+typedef htri_t tri_t;
 
 } // namespace IO
 } // namespace HIPP
+
+
 #endif	//_HIPPIO_H5_BASE_H_

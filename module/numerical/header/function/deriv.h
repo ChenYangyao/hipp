b4fdaf4c32347d3ee19936834da8c6ff8b1309c4
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-01-06 15:06:39 -0500

    initalize deriv

--- module/numerical/header/function/deriv.h
+++ module/numerical/header/function/deriv.h
@@ -10,14 +10,14 @@ public:
     typedef GSLFn::user_fn_t fn_t;
     typedef double value_t;
     
-    Deriv( fn_t fn = &GSLFn::_null_user_fn, void *extra_data = nullptr );
+    Deriv( fn_t fn = fn_t() );
     _HIPPNUMERICAL_GSL_UTIL_DEFAULT_CP_MV(Deriv)
     ~Deriv();
 
-    Deriv & set_fn( fn_t fn = &GSLFn::_null_user_fn, 
-        void *extra_data = nullptr );
+    Deriv & set_fn( fn_t fn = fn_t());
 
-    fn_t & fn( void * &extra_data ) noexcept;
+    fn_t & fn() noexcept;
+    const fn_t & fn() const noexcept;
 
     double operator()( double x, double h );
     double forward( double x, double h );
@@ -28,11 +28,46 @@ public:
     int last_errno() const noexcept;
 protected:
     GSLFn _gsl_fn;
-
     double _result = 0., _err_abs = 0.;
     int _last_errno = 0;
 };
 
+inline Deriv::Deriv( fn_t fn ): _gsl_fn(std::move(fn)){
+}
+inline Deriv::~Deriv() {}
+
+inline Deriv & Deriv::set_fn( fn_t fn ){
+    _gsl_fn.set_fn( std::move(fn) );
+    return *this;
+}
+
+inline Deriv::fn_t & Deriv::fn() noexcept{
+    return _gsl_fn.get_user_fn();
+}
+
+inline const Deriv::fn_t & Deriv::fn() const noexcept{
+    return _gsl_fn.get_user_fn();
+}
+
+inline double Deriv::operator()( double x, double h ){
+    _last_errno = gsl_deriv_central( 
+        &_gsl_fn.get_gsl_fn(), x, h, &_result, &_err_abs );
+    return _result;
+}
+inline double Deriv::forward( double x, double h ){
+    _last_errno = gsl_deriv_forward( 
+        &_gsl_fn.get_gsl_fn(), x, h, &_result, &_err_abs );
+    return _result;
+}
+inline double Deriv::backward( double x, double h ){
+    _last_errno = gsl_deriv_backward( 
+        &_gsl_fn.get_gsl_fn(), x, h, &_result, &_err_abs );
+    return _result;
+}
+
+inline double Deriv::result() const noexcept{ return _result; }
+inline double Deriv::err_abs() const noexcept{ return _err_abs; }
+inline int Deriv::last_errno() const noexcept{ return _last_errno; }
 
 } // namespace NUMERICAL
 } // namespace HIPP

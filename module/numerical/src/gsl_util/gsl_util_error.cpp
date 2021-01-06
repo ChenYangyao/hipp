19c1ea8423241a7b16d41a18596800c0b7d786d0
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-01-06 15:07:36 -0500

    new: gsl function wrappers

--- module/numerical/src/gsl_util/gsl_util_error.cpp
+++ module/numerical/src/gsl_util/gsl_util_error.cpp
@@ -2,7 +2,9 @@
 namespace HIPP{
 namespace NUMERICAL{
 
-int ErrGSL::_has_init_gsl_env = 0;
+ErrGSL::err_handler_t * ErrGSL::_lib_default_err_handler 
+    = ErrGSL::set_error_handler();
+int ErrGSL::_has_init_gsl_env = 1;
 
 } // namespace NUMERICAL
 } // namespace HIPP

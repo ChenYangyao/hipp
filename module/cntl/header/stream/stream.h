ed90a4067c6b635d60d3444756582b26e77683cb
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-06-02 00:43:52 -0400

    add stream for pretty log

--- module/cntl/header/stream/stream.h
+++ module/cntl/header/stream/stream.h
@@ -1,13 +1,14 @@
 /**
- * creat: Yangyao CHEN, 2019/12/28
- * The total header file composed of all headers in this 'stream' module.
- */ 
+create: Yangyao CHEN, 2019/12/28
+The total header file composed of all headers in this submodule.
+*/
 
 #ifndef _HIPPCNTL_STREAM_H_
 #define _HIPPCNTL_STREAM_H_
 
 #include "stream_fmt_io.h"
 #include "stream_pretty.h"
+#include "stream_pretty_log.h"
 
 #include "stream_str.h"
 #endif	//_HIPPCNTL_STREAM_H_

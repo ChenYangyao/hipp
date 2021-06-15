cd101147978de489fa0c75fb7d05b5f6251bda33
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-06-15 01:00:48 -0400

    add new distributions

--- module/hippnumerical/header/hippnumerical_random_number/randist.h
+++ module/hippnumerical/header/hippnumerical_random_number/randist.h
@@ -2,5 +2,10 @@
 #define _HIPPNUMERICAL_RANDIST_H_
 
 #include "randist_gaussian.h"
+#include "randist_gaussian_tail.h"
+#include "randist_bivariate_gaussian.h"
+#include "randist_lognormal.h"
+
+#include "randist_uniform_real.h"
 
 #endif	//_HIPPNUMERICAL_RANDIST_H_

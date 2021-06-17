092f6ebfcb62a8fdbd5d0d4e1ce87f0f33d75bef
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-06-16 23:06:19 -0400

    Move the implementation outside class def.

--- module/hippnumerical/header/hippnumerical_gsl_util/gsl_util_error.h
+++ module/hippnumerical/header/hippnumerical_gsl_util/gsl_util_error.h
@@ -4,62 +4,105 @@
 namespace HIPP{
 namespace NUMERICAL{
 
+/**
+ErrGSL - the exception thrown from the GSL error.
+
+When linking with the HIPP numeric library, GSL error handler is set to the 
+HIPP default, i.e.,  a short message is printed and an exception typed ErrGSL 
+is thrown. User can turn off it by calling the method set_err_handler_off(), 
+or change the handler by set_err_handler(new_handler).
+*/
 class ErrGSL: public ErrType<ErrAppGSL, ErrClassDefault>{
 public:
     typedef ErrType<ErrAppGSL, ErrClassDefault> err_type_t;
     typedef gsl_error_handler_t err_handler_t;
     using err_type_t::ErrType;
 
-    virtual const char *what() const noexcept override { 
-        return gsl_strerror( _errno ); 
-    }
-    virtual string whats() const override{
-        return err_type_t::whats() + " | Type: " + what(); }
+    virtual const char * what() const noexcept override;
+    virtual string whats() const override;
     
     static void default_err_handler( 
-        const char *reason, const char *file, int line, int gsl_errno ){
-        if( _err_cntl_flag )
-            prints( "gsl: ", file, ":", line, ": ERROR: ", reason, '\n',
-            "Default HIPP GSL error hanlder invoked.\nErrGSL thrown" );
-        throw ErrGSL( gsl_errno );   
-    }
-    const char *strerror(const int gsl_errno) const noexcept {
-        return gsl_strerror(gsl_errno);
-    }
+        const char *reason, const char *file, int line, int gsl_errno);
+    const char * strerror(const int gsl_errno) const noexcept;
     static err_handler_t * set_error_handler( 
-        err_handler_t *new_hanlder = &default_err_handler ) noexcept {
-        return gsl_set_error_handler( new_hanlder );
-    }
-    static err_handler_t * set_error_handler_off() noexcept{
-        return gsl_set_error_handler_off();
-    }
+        err_handler_t *new_handler = &default_err_handler ) noexcept;
+    static err_handler_t * set_error_handler_off() noexcept;
 
     template<typename ...Args>
-    static void throw_( errno_t gsl_errno, Args &&... args ){
-        if( _err_cntl_flag )
-            prints( std::forward<Args>(args)... );
-        throw ErrGSL( gsl_errno ); 
-    }
+    static void throw_( errno_t gsl_errno, Args &&... args);
     template<typename ...Args>
-    static void abort( errno_t e, Args &&... args ){
-        prints( std::forward<Args>(args)... );
-        exit( e );
-    }
+    static void abort( errno_t e, Args &&... args);
     template<typename ...Args>
-    static void check( errno_t gsl_errno, Args &&... args ) { 
-        if( gsl_errno ) 
-            throw_(gsl_errno, std::forward<Args>(args)...); 
-    }
+    static void check( errno_t gsl_errno, Args &&... args);
+    template<typename ...Args>
+    static void check_alloc( void *ptr, Args &&... args );
 
-    template<typename ...Args>
-    static void check_alloc( void *ptr, Args &&... args ){
-        if( !ptr )
-            throw_(GSL_ENOMEM, std::forward<Args>(args)...);
-    }
-
+protected:
     static int _has_init_gsl_env;
     static err_handler_t * _lib_default_err_handler;
 };
+
+inline 
+const char * ErrGSL::what() const noexcept { 
+    return gsl_strerror( _errno ); 
+}
+
+
+/** Implementation */
+inline 
+string ErrGSL::whats() const {
+    return err_type_t::whats() + " | Type: " + what(); }
+
+inline 
+void ErrGSL::default_err_handler( 
+    const char *reason, const char *file, int line, int gsl_errno ){
+    if( _err_cntl_flag )
+        prints( "gsl: ", file, ":", line, ": ERROR: ", reason, '\n',
+        "Default HIPP GSL error hadler invoked.\nErrGSL thrown" );
+    throw ErrGSL( gsl_errno );   
+}
+
+inline 
+const char * ErrGSL::strerror(const int gsl_errno) const noexcept {
+    return gsl_strerror(gsl_errno);
+}
+
+inline 
+auto ErrGSL::set_error_handler( err_handler_t *new_handler) noexcept 
+-> err_handler_t * 
+{
+    return gsl_set_error_handler( new_handler );
+}
+
+inline 
+auto ErrGSL::set_error_handler_off() noexcept -> err_handler_t * {
+    return gsl_set_error_handler_off();
+}
+
+template<typename ...Args>
+void ErrGSL::throw_( errno_t gsl_errno, Args &&... args ){
+    if( _err_cntl_flag )
+        prints( std::forward<Args>(args)... );
+    throw ErrGSL( gsl_errno ); 
+}
+
+template<typename ...Args>
+void ErrGSL::abort( errno_t e, Args &&... args ){
+    prints( std::forward<Args>(args)... );
+    exit( e );
+}
+
+template<typename ...Args>
+void ErrGSL::check( errno_t gsl_errno, Args &&... args ) { 
+    if( gsl_errno ) 
+        throw_(gsl_errno, std::forward<Args>(args)...); 
+}
+
+template<typename ...Args>
+void ErrGSL::check_alloc( void *ptr, Args &&... args ){
+    if( !ptr )
+        throw_(GSL_ENOMEM, std::forward<Args>(args)...);
+}
 
 
 

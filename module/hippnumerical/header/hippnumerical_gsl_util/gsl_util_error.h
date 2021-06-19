64c327be2e0db36dc2afcd47a83fe9facf8bea26
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-06-19 00:27:19 -0400

    Add error numbers and comments.

--- module/hippnumerical/header/hippnumerical_gsl_util/gsl_util_error.h
+++ module/hippnumerical/header/hippnumerical_gsl_util/gsl_util_error.h
@@ -14,6 +14,11 @@ or change the handler by set_err_handler(new_handler).
 */
 class ErrGSL: public ErrType<ErrAppGSL, ErrClassDefault>{
 public:
+    enum : int { 
+        SUCCESS_ = GSL_SUCCESS, EDOM_ = GSL_EDOM,     ERANGE_ = GSL_ERANGE, 
+        ENOMEM_ = GSL_ENOMEM,   EINVAL_ = GSL_EINVAL
+    };
+
     typedef ErrType<ErrAppGSL, ErrClassDefault> err_type_t;
     typedef gsl_error_handler_t err_handler_t;
     using err_type_t::ErrType;
@@ -21,22 +26,40 @@ public:
     virtual const char * what() const noexcept override;
     virtual string whats() const override;
     
+    /**
+    The default error handler prints a message and throw ErrGSL(gsl_errno).
+    */
     static void default_err_handler( 
         const char *reason, const char *file, int line, int gsl_errno);
-    const char * strerror(const int gsl_errno) const noexcept;
+    /**
+    Set a specific error handler, or turn it off.
+    */
     static err_handler_t * set_error_handler( 
         err_handler_t *new_handler = &default_err_handler ) noexcept;
     static err_handler_t * set_error_handler_off() noexcept;
 
+    /** Query error string of predefined GSL error number. */
+    const char * strerror(const int gsl_errno) const noexcept;
+
+    /**
+    throw_() - throw an error. If error_cntl_flag is ON, print args before 
+        throwing.
+    abort() - always print args and exit with provided error number.
+    */
     template<typename ...Args>
     static void throw_( errno_t gsl_errno, Args &&... args);
     template<typename ...Args>
     static void abort( errno_t e, Args &&... args);
+
+    /**
+    Check for general errors and specific error. 
+    check() - if not success, call throw_(gsl_errno, args...).
+    check_alloc() - if ptr is NULL, call throw_(ENOMEM_, args...).
+    */
     template<typename ...Args>
     static void check( errno_t gsl_errno, Args &&... args);
     template<typename ...Args>
     static void check_alloc( void *ptr, Args &&... args );
-
 protected:
     static int _has_init_gsl_env;
     static err_handler_t * _lib_default_err_handler;
@@ -44,9 +67,8 @@ protected:
 
 inline 
 const char * ErrGSL::what() const noexcept { 
-    return gsl_strerror( _errno ); 
+    return strerror(_errno);
 }
-
 
 /** Implementation */
 inline 
@@ -63,11 +85,6 @@ void ErrGSL::default_err_handler(
 }
 
 inline 
-const char * ErrGSL::strerror(const int gsl_errno) const noexcept {
-    return gsl_strerror(gsl_errno);
-}
-
-inline 
 auto ErrGSL::set_error_handler( err_handler_t *new_handler) noexcept 
 -> err_handler_t * 
 {
@@ -77,6 +94,11 @@ auto ErrGSL::set_error_handler( err_handler_t *new_handler) noexcept
 inline 
 auto ErrGSL::set_error_handler_off() noexcept -> err_handler_t * {
     return gsl_set_error_handler_off();
+}
+
+inline 
+const char * ErrGSL::strerror(const int gsl_errno) const noexcept {
+    return gsl_strerror(gsl_errno);
 }
 
 template<typename ...Args>
@@ -94,14 +116,14 @@ void ErrGSL::abort( errno_t e, Args &&... args ){
 
 template<typename ...Args>
 void ErrGSL::check( errno_t gsl_errno, Args &&... args ) { 
-    if( gsl_errno ) 
+    if( gsl_errno != SUCCESS_ ) 
         throw_(gsl_errno, std::forward<Args>(args)...); 
 }
 
 template<typename ...Args>
 void ErrGSL::check_alloc( void *ptr, Args &&... args ){
     if( !ptr )
-        throw_(GSL_ENOMEM, std::forward<Args>(args)...);
+        throw_(ENOMEM_, std::forward<Args>(args)...);
 }
 
 

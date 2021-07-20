8e04929fa72e710f0b01d5e6af62ea069ccd4c86
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-07-20 12:40:04 -0400

    Add get_elem

--- module/hippcntl/header/hippcntl_generic/concept_raw_array.h
+++ module/hippcntl/header/hippcntl_generic/concept_raw_array.h
@@ -42,6 +42,19 @@ public:
     }
 
     /**
+    Get an element from a raw array typed object.
+    e.g., int[3][4] &a, 0, 1 => a[0][1].
+        int &a => a.
+    */
+    template<typename ArrayT>
+    ArrayT & get_elem(ArrayT &a) { return a; }
+
+    template<typename ArrayT, typename ...SizeTs>
+    std::remove_all_extents_t<ArrayT> & get_elem(ArrayT &a, size_t id1, SizeTs &&...ids) {
+        return get_elem(a[id1], std::forward<SizeTs>(ids)...);
+    }
+
+    /**
     Get raw array type from from the extents list.
     e.g.,
     int, 3, 4, 5 => int[3][4][5].

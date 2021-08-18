b8eeac4274ae6aa8b125032ac325a4b73943fbc3
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-08-18 04:51:54 -0400

    Add more random engine types

--- module/hippnumerical/header/hippnumerical_random_number/rannum_base.h
+++ module/hippnumerical/header/hippnumerical_random_number/rannum_base.h
@@ -11,7 +11,32 @@ create: Yangyao CHEN, 2021/08/12
 
 namespace HIPP::NUMERICAL {
 
-typedef std::default_random_engine DefaultRandomEngine;
+struct RandomEngine {
+    /**
+    Predefined random number generator types.
+    */
+    using minstd_rand0_t          = std::minstd_rand0;
+    using minstd_rand_t           = std::minstd_rand;
+    using mt19937_t               = std::mt19937;
+    using mt19937_64_t            = std::mt19937_64;
+    using ranlux24_base_t         = std::ranlux24_base;
+    using ranlux48_base_t         = std::ranlux48_base;
+    using ranlux24_t              = std::ranlux24;
+    using ranlux48_t              = std::ranlux48;
+    using knuth_b_t               = std::knuth_b;
+    using default_t               = std::default_random_engine;
+    
+    /**
+    Non-deterministic random number generator types.
+    */
+    using device_t                = std::random_device;
+};
+
+/**
+The default one chosen by the implementation. Generally the best in most cases.
+*/
+typedef RandomEngine::default_t DefaultRandomEngine;
+
 
 /**
 The default random engine objects used by HIPP.

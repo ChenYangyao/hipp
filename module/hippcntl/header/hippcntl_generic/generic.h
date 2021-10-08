bd160f83fc385d29b3bb78222537df3ee3892303
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-10-08 15:54:50 -0400

    add concept_dynamic_array inclusion

--- module/hippcntl/header/hippcntl_generic/generic.h
+++ module/hippcntl/header/hippcntl_generic/generic.h
@@ -1,5 +1,6 @@
 #ifndef _HIPPCNTL_GENERIC_H_
 #define _HIPPCNTL_GENERIC_H_
 #include "concept_raw_array.h"
+#include "concept_dynamic_array.h"
 #include "concept_contiguous_buffer.h"
 #endif	//_HIPPCNTL_GENERIC_H_

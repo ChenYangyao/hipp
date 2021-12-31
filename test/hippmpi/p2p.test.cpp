b2d42eafe5e08f34bc6092f08a6dae98d442aec0
Yangyao Chen <yangyaochen.astro@foxmail.com>
2021-12-31 10:51:11 +0800

    Add tests for sendrecv and probes

--- test/hippmpi/p2p.test.cpp
+++ test/hippmpi/p2p.test.cpp
@@ -1,5 +1,3 @@
-#include <testdata.h>
-#include <testmacro.h>
 #include <mpi_test_incl.h>
 #include <mpi_test_data_buffer.h>
 
@@ -16,14 +14,24 @@ struct PointToPointTest : MPITestFixture {
         HIPPMPI_TEST_F_ADD_CASE(blocking_synchronous);
         HIPPMPI_TEST_F_ADD_CASE(blocking_buffered);
         HIPPMPI_TEST_F_ADD_CASE(blocking_ready);
+
         HIPPMPI_TEST_F_ADD_CASE(nonblocking_standard);
         HIPPMPI_TEST_F_ADD_CASE(nonblocking_synchronous);
         HIPPMPI_TEST_F_ADD_CASE(nonblocking_buffered);
         HIPPMPI_TEST_F_ADD_CASE(nonblocking_ready);
+        
         HIPPMPI_TEST_F_ADD_CASE(persistent_standard);
         HIPPMPI_TEST_F_ADD_CASE(persistent_synchronous);
         HIPPMPI_TEST_F_ADD_CASE(persistent_buffered);
         HIPPMPI_TEST_F_ADD_CASE(persistent_ready);
+        
+        HIPPMPI_TEST_F_ADD_CASE(sendrecv);
+        HIPPMPI_TEST_F_ADD_CASE(sendrecv_replace);
+
+        HIPPMPI_TEST_F_ADD_CASE(probe);
+        HIPPMPI_TEST_F_ADD_CASE(iprobe);
+        HIPPMPI_TEST_F_ADD_CASE(mprobe);
+        HIPPMPI_TEST_F_ADD_CASE(improbe);
     }
     void blocking_standard(){
         data_buffer_t b(*this), b_in(*this, false);
@@ -459,6 +467,406 @@ struct PointToPointTest : MPITestFixture {
                 req.waitall();
                 b.expect_eq(b_in);
             }
+        }
+    }
+
+    void sendrecv() {
+        int dims[] = {n_procs}, periods[] = {1};
+        auto ring = comm.cart_create(dims, periods, 0);
+
+        auto [l,r] = ring.cart_shift(0);
+        data_buffer_t b(*this), b_in(*this, false);
+        for(int i=0; i<10; ++i){
+            b_in.zero_values();
+            ring.sendrecv(b.x_s, r, 0,          b_in.x_s, l, 0);
+            ring.sendrecv(b.x_rawarr3, r, 0,    b_in.x_rawarr3, l, 0);
+            ring.sendrecv(b.x_rawarr34, r, 0,   b_in.x_rawarr34, l, 0);
+            ring.sendrecv(b.x_arr3, r, 0,       b_in.x_arr3, l, 0);
+            ring.sendrecv(b.x_arr34, r, 0,      b_in.x_arr34, l, 0);
+            ring.sendrecv(b.x_v8, r, 0,         b_in.x_v8, l, 0);
+            ring.sendrecv(b.x_v83, r, 0,        b_in.x_v83, l, 0);
+            ring.sendrecv(b.x_v834, r, 0,       b_in.x_v834, l, 0);
+            ring.sendrecv({b.x_p8, 8}, r, 0,    b_in.x_p8, l, 0);
+            ring.sendrecv({b.x_p8, 8, Datatype::from_type<scalar_t>()}, 
+                                          r, 0, b_in.x_p8, l, 0);
+            b.expect_eq(b_in);
+            std::swap(l, r);
+        }
+    }
+    void sendrecv_replace(){
+        int dims[] = {n_procs}, periods[] = {1};
+        auto ring = comm.cart_create(dims, periods, 0);
+
+        auto [l,r] = ring.cart_shift(0);
+        data_buffer_t b(*this), b_in(*this, false);
+        for(int i=0; i<10; ++i){
+            b_in.init_values();
+            ring.sendrecv_replace(b_in.x_s, r, 0,        l, 0);
+            ring.sendrecv_replace(b_in.x_rawarr3, r, 0,  l, 0);
+            ring.sendrecv_replace(b_in.x_rawarr34, r, 0, l, 0);
+            ring.sendrecv_replace(b_in.x_arr3, r, 0,     l, 0);
+            ring.sendrecv_replace(b_in.x_arr34, r, 0,    l, 0);
+            ring.sendrecv_replace(b_in.x_v8, r, 0,       l, 0);
+            ring.sendrecv_replace(b_in.x_v83, r, 0,      l, 0);
+            ring.sendrecv_replace(b_in.x_v834, r, 0,     l, 0);
+            ring.sendrecv_replace({b_in.x_p8, 8}, r, 0,  l, 0);
+            ring.sendrecv_replace({b_in.x_p8, 8, 
+                Datatype::from_type<scalar_t>()}, r, 0,  l, 0);
+            b.expect_eq(b_in);
+            std::swap(l, r);
+        }
+    }
+
+    void probe() {
+        data_buffer_t b(*this), b_in(*this, false);
+        if( rank == 0 ) {
+            for(int i=1; i<n_procs; ++i){
+                comm.send(i, 0, b.x_s);
+                comm.send(i, 0, b.x_rawarr3);
+                comm.send(i, 0, b.x_rawarr34);
+                comm.send(i, 0, b.x_arr3);
+                comm.send(i, 0, b.x_arr34);
+                comm.send(i, 0, b.x_v8);
+                comm.send(i, 0, b.x_v83);
+                comm.send(i, 0, b.x_v834);
+                comm.send(i, 0, b.x_p8, 8);
+                comm.send(i, 0, b.x_p8, 8, Datatype::from_type<scalar_t>());
+            }
+        } else {
+                auto & dt = Datatype::from_type<scalar_t>();
+                Status st;
+                
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 1);
+                comm.recv(0, 0, b_in.x_s);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                comm.recv(0, 0, b_in.x_rawarr3);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                comm.recv(0, 0, b_in.x_rawarr34);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                comm.recv(0, 0, b_in.x_arr3);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                comm.recv(0, 0, b_in.x_arr34);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                comm.recv(0, 0, b_in.x_v8);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 24);
+                comm.recv(0, 0, b_in.x_v83);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 96);
+                comm.recv(0, 0, b_in.x_v834);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                comm.recv(0, 0, b_in.x_p8, 8);
+
+                st = comm.probe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                comm.recv(0, 0, b_in.x_p8, 8, Datatype::from_type<scalar_t>());
+
+                b.expect_eq(b_in);
+        }
+    }
+    void iprobe() {
+        data_buffer_t b(*this), b_in(*this, false);
+        if( rank == 0 ) {
+            for(int i=1; i<n_procs; ++i){
+                comm.send(i, 0, b.x_s);
+                comm.send(i, 0, b.x_rawarr3);
+                comm.send(i, 0, b.x_rawarr34);
+                comm.send(i, 0, b.x_arr3);
+                comm.send(i, 0, b.x_arr34);
+                comm.send(i, 0, b.x_v8);
+                comm.send(i, 0, b.x_v83);
+                comm.send(i, 0, b.x_v834);
+                comm.send(i, 0, b.x_p8, 8);
+                comm.send(i, 0, b.x_p8, 8, Datatype::from_type<scalar_t>());
+            }
+        } else {
+                auto & dt = Datatype::from_type<scalar_t>();
+                Status st;
+                int flag;
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 1);
+                comm.recv(0, 0, b_in.x_s);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                comm.recv(0, 0, b_in.x_rawarr3);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                comm.recv(0, 0, b_in.x_rawarr34);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                comm.recv(0, 0, b_in.x_arr3);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                comm.recv(0, 0, b_in.x_arr34);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                comm.recv(0, 0, b_in.x_v8);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 24);
+                comm.recv(0, 0, b_in.x_v83);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 96);
+                comm.recv(0, 0, b_in.x_v834);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                comm.recv(0, 0, b_in.x_p8, 8);
+
+                flag = 0;
+                do{
+                    st = comm.iprobe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                comm.recv(0, 0, b_in.x_p8, 8, Datatype::from_type<scalar_t>());
+
+                b.expect_eq(b_in);
+        }
+    }
+    void mprobe() {
+        data_buffer_t b(*this), b_in(*this, false);
+        if( rank == 0 ) {
+            for(int i=1; i<n_procs; ++i){
+                comm.send(i, 0, b.x_s);
+                comm.send(i, 0, b.x_rawarr3);
+                comm.send(i, 0, b.x_rawarr34);
+                comm.send(i, 0, b.x_arr3);
+                comm.send(i, 0, b.x_arr34);
+                comm.send(i, 0, b.x_v8);
+                comm.send(i, 0, b.x_v83);
+                comm.send(i, 0, b.x_v834);
+                comm.send(i, 0, b.x_p8, 8);
+                comm.send(i, 0, b.x_p8, 8, Datatype::from_type<scalar_t>());
+            }
+        } else {
+                auto & dt = Datatype::from_type<scalar_t>();
+                Status st; Message msg; Requests req;
+                
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 1);
+                msg.mrecv(b_in.x_s);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                msg.mrecv(b_in.x_rawarr3);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                msg.mrecv(b_in.x_rawarr34);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                msg.mrecv(b_in.x_arr3);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                msg.mrecv(b_in.x_arr34);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                req += msg.imrecv(b_in.x_v8);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 24);
+                req += msg.imrecv(b_in.x_v83);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 96);
+                req += msg.imrecv(b_in.x_v834);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                req += msg.imrecv(b_in.x_p8, 8);
+
+                std::tie(st, msg) = comm.mprobe(0, 0);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                req += msg.imrecv(b_in.x_p8, 8, Datatype::from_type<scalar_t>());
+
+                req.waitall();
+                b.expect_eq(b_in);
+        }
+    }
+    void improbe() {
+        data_buffer_t b(*this), b_in(*this, false);
+        if( rank == 0 ) {
+            for(int i=1; i<n_procs; ++i){
+                comm.send(i, 0, b.x_s);
+                comm.send(i, 0, b.x_rawarr3);
+                comm.send(i, 0, b.x_rawarr34);
+                comm.send(i, 0, b.x_arr3);
+                comm.send(i, 0, b.x_arr34);
+                comm.send(i, 0, b.x_v8);
+                comm.send(i, 0, b.x_v83);
+                comm.send(i, 0, b.x_v834);
+                comm.send(i, 0, b.x_p8, 8);
+                comm.send(i, 0, b.x_p8, 8, Datatype::from_type<scalar_t>());
+            }
+        } else {
+                auto & dt = Datatype::from_type<scalar_t>();
+                Status st; Message msg; Requests req;
+                int flag;
+                
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 1);
+                msg.mrecv(b_in.x_s);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                msg.mrecv(b_in.x_rawarr3);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                msg.mrecv(b_in.x_rawarr34);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 3);
+                msg.mrecv(b_in.x_arr3);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 12);
+                msg.mrecv(b_in.x_arr34);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                req += msg.imrecv(b_in.x_v8);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 24);
+                req += msg.imrecv(b_in.x_v83);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 96);
+                req += msg.imrecv(b_in.x_v834);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                req += msg.imrecv(b_in.x_p8, 8);
+
+                flag = 0;
+                do {
+                    std::tie(st, msg) = comm.improbe(0, 0, flag);
+                } while(!flag);
+                expect_eq(st.source(), 0); expect_eq(st.tag(), 0);
+                expect_eq(st.count(dt), 8);
+                req += msg.imrecv(b_in.x_p8, 8, Datatype::from_type<scalar_t>());
+
+                req.waitall();
+                b.expect_eq(b_in);
         }
     }
 };

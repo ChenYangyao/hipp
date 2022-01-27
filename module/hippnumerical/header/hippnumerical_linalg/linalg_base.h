73c4387782ad9717f45dab0c82211aacbbaaafbe
Yangyao Chen <yangyaochen.astro@foxmail.com>
2022-01-28 03:41:15 +0800

    Remove redundant size_t type alias

--- module/hippnumerical/header/hippnumerical_linalg/linalg_base.h
+++ module/hippnumerical/header/hippnumerical_linalg/linalg_base.h
@@ -1,10 +1,9 @@
 #ifndef _HIPPNUMERICAL_LINALG_BASE_H_
 #define _HIPPNUMERICAL_LINALG_BASE_H_
+
 #include <hippcntl.h>
 #include <limits>
 #include <type_traits>
 #include <tuple>
-namespace HIPP::NUMERICAL {
-typedef std::size_t size_t;
-} // namespace HIPP::NUMERICAL
+
 #endif	//_HIPPNUMERICAL_LINALG_BASE_H_

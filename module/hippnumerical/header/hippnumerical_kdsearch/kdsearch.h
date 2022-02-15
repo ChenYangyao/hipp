70b6699f99d94bdab19e8b548190c5c2b23bece4
Yangyao Chen <yangyaochen.astro@foxmail.com>
2022-02-15 13:07:58 +0800

    initialize kdsearch

---
+++ module/hippnumerical/header/hippnumerical_kdsearch/kdsearch.h
@@ -0,0 +1,12 @@
+/**
+create: Yangyao CHEN, 2022/02/14
+The header of the space searching algorithms of NUMERICAL module.
+*/
+
+#ifndef _HIPPNUMERICAL_KDSEARCH_H_
+#define _HIPPNUMERICAL_KDSEARCH_H_
+
+#include "kdsearch_kdmesh.h"
+#include "kdsearch_kdtree.h"
+
+#endif	//_HIPPNUMERICAL_KDSEARCH_H_

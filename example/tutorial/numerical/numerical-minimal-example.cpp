e790e65e8a05a801fd1dc35a1ca21ee21ffc9a7b
Yangyao Chen <yangyaochen.astro@foxmail.com>
2022-07-07 17:31:41 +0800

    doc KDTree

---
+++ example/tutorial/numerical/numerical-minimal-example.cpp
@@ -0,0 +1,13 @@
+// numerical-minimal-example.cpp
+
+#include <hippnumerical.h>                  // [1]
+
+using namespace HIPP;
+using namespace std;
+namespace nu = HIPP::NUMERICAL;             // [2]
+
+int main(int argc, char const *argv[])
+{
+    // use the library
+    return 0;
+}
